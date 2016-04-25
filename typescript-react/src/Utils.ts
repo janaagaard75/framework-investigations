38c0a1d6e90a09004ccc853681526d829796a4db
Jan Aagaard <jag@zmags.com>
2016-04-25 16:10:55 +0200

    Sorted functions alphabetically

--- typescript-react/src/Utils.ts
+++ typescript-react/src/Utils.ts
@@ -1,18 +1,15 @@
 export default class Utils {
-  public static uuid(): string {
-    /* tslint:disable:no-bitwise */
-    let uuid = "";
-
-    for (let i = 0; i < 32; i++) {
-      const random = Math.random() * 16 | 0;
-      if (i === 8 || i === 12 || i === 16 || i === 20) {
-        uuid += "-";
+  public static extend(...objs: any[]): any {
+    const newObj = {};
+    for (let i = 0; i < objs.length; i++) {
+      const obj = objs[i];
+      for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          newObj[key] = obj[key];
+        }
       }
-      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
     }
-
-    return uuid;
-    /* tslint:enable */
+    return newObj;
   }
 
   public static pluralize(count: number, word: string) {
@@ -28,16 +25,19 @@ export default class Utils {
     return (store && JSON.parse(store)) || [];
   }
 
-  public static extend(...objs: any[]): any {
-    const newObj = {};
-    for (let i = 0; i < objs.length; i++) {
-      const obj = objs[i];
-      for (let key in obj) {
-        if (obj.hasOwnProperty(key)) {
-          newObj[key] = obj[key];
-        }
+  public static uuid(): string {
+    /* tslint:disable:no-bitwise */
+    let uuid = "";
+
+    for (let i = 0; i < 32; i++) {
+      const random = Math.random() * 16 | 0;
+      if (i === 8 || i === 12 || i === 16 || i === 20) {
+        uuid += "-";
       }
+      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
     }
-    return newObj;
+
+    return uuid;
+    /* tslint:enable */
   }
 }
