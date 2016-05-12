c2b4858c3f443845687b52da88527b802581fe37
Jan Aagaard <jan@aagaard.net>
2016-05-12 07:57:52 +0200

    Generate index.html using HtmlWebpackPlugin

--- typescript-marionette-v2/webpack.config.js
+++ typescript-marionette-v2/webpack.config.js
@@ -1,6 +1,16 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require("webpack");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
+    path: "dist",
     filename: "bundle.js"
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.ejs',
+      title: 'Todos'
+    })
+  ]
 }
