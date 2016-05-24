e248aea8a2f65cb0bd74e0bb8420621c74e652ba
Jan Aagaard <jan@aagaard.net>
2016-05-24 22:37:21 +0200

    Enable TypeScript in Webpack

--- typescript-marionette-v2/src/App.ts
+++ typescript-marionette-v2/src/App.ts
@@ -1,3 +1,4 @@
+// The reference only needed by VS Code, and not by the TypeScript compiler.
 /// <reference path="../typings/index.d.ts"/>
 
 import * as Marionette from "backbone.marionette"
--- typescript-marionette-v2/webpack.config.js
+++ typescript-marionette-v2/webpack.config.js
@@ -1,8 +1,20 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require("webpack");
 
 module.exports = {
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
   entry: "./src/index.js",
+  module: {
+    loaders: [
+      // All files with a .ts extension will be handled by ts-loader.
+      { test: /\.ts$/, loader: "ts-loader" }
+    ],
+
+    preLoaders: [
+      // All output .js files will have any sourcemaps re-processed by source-map-loader.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  },
   output: {
     path: "dist",
     filename: "bundle.js"
@@ -12,5 +24,9 @@ module.exports = {
       template: './src/index.ejs',
       title: 'Todos'
     })
-  ]
+  ],
+  resolve: {
+    // Add .ts as resolvable extensions.
+    extensions: ["", ".webpack.js", ".web.js", ".ts", ".js"]
+  }
 }
