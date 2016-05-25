b8b984daeadb7a4919af067b6ad00a3ee8bbc395
Jan Aagaard <jag@zmags.com>
2016-05-25 14:29:46 +0200

    Start Backbone's history when the app is started

--- typescript-marionette-v2/src/index.ts
+++ typescript-marionette-v2/src/index.ts
@@ -4,9 +4,9 @@ import App from "App"
 const app = new App()
 
 // Start history when our application is ready
-// app.on("start", () => {
-//   Backbone.history.start()
-// })
+app.on("start", () => {
+  Backbone.history.start()
+})
 
 // Load some initial data, and then start our application
 //loadInitialData().then(app.start);
