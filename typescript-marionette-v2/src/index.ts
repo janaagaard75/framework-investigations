e689175b0e36cffffb4964718e98ec5500af6ecb
Jan Aagaard <jag@zmags.com>
2016-06-02 08:39:18 +0200

    Cleanup

--- typescript-marionette-v2/src/index.ts
+++ typescript-marionette-v2/src/index.ts
@@ -13,13 +13,13 @@ const app = new App()
 
 // TODO: Move as much as possible into App.ts.
 
-// Start history when our application is ready
+// Start history when our application is ready.
 app.on("start", () => {
   window.console.info("Application started.")
   Backbone.history.start()
 })
 
-// Load some initial data, and then start our application
+// Load some initial data, and then start our application.
 //loadInitialData().then(app.start);
 //app.start({container: "#root"})
 app.start()
