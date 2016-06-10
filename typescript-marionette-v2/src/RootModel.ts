b0184b2a306b2e4769e98f1b31b64e7b32b862f4
Jan Aagaard <jag@zmags.com>
2016-06-10 18:45:27 +0200

    Kinda fixed importing Backbone.LocalStorage

--- typescript-marionette-v2/src/RootModel.ts
+++ typescript-marionette-v2/src/RootModel.ts
@@ -1,3 +1,9 @@
+// Requires the following added to the bottom of typings/global/backbone.localstorage/index.d.ts.
+// declare module "backbone.localstorage" {
+//     export = Backbone.LocalStorage;
+// }
+import Store = require("backbone.localstorage")
+
 export default class RootModel extends Backbone.Model {
   defaults() {
     return {
@@ -5,7 +11,7 @@ export default class RootModel extends Backbone.Model {
     }
   }
 
-  localStorage = new Backbone.LocalStorage("todos-typescript-marionette-v2")
+  localStorage = new Store("todos-typescript-marionette-v2")
 
   get title(): string {
     return this.get("title")
