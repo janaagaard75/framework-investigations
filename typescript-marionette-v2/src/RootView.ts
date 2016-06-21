727103c0cbb7fc71621d7d2f7d9da431e089b136
Jan Aagaard <jag@zmags.com>
2016-06-21 13:51:32 +0200

    Avoid IDs in components

--- typescript-marionette-v2/src/RootView.ts
+++ typescript-marionette-v2/src/RootView.ts
@@ -1,19 +1,21 @@
 import * as Marionette from "backbone.marionette"
 import RootModel from "./RootModel"
 
-interface RootViewOptions extends Backbone.ViewOptions<RootModel> {
+interface RootViewOptions extends Marionette.LayoutViewOptions<RootModel> {
 }
 
-export default class RootView extends Marionette.ItemView<RootModel> {
+export default class RootView extends Marionette.LayoutView<RootModel> {
   constructor(options: RootViewOptions) {
-    super(RootView.setPlaceholderElement(options))
+    super(RootView.setDefaultOptions(options))
   }
 
   template = require("./RootView.ejs")
 
-  private static setPlaceholderElement(options: RootViewOptions): RootViewOptions {
-    if (!options.el) {
-      options.el = "#rootPlaceholder"
+  private static setDefaultOptions(options: RootViewOptions): RootViewOptions {
+    options.el = ".jsRootPlaceholder"
+
+    options.regions = {
+      todos: "jsTodosPlaceholder"
     }
 
     return options
