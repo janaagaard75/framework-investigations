3f3bd90ab545d0a721b669ad6d271e9cbe0e1a25
Jan Aagaard <jag@zmags.com>
2016-06-24 13:23:40 +0200

    Add TODO

--- typescript-marionette-v2/src/TodoCollection.ts
+++ typescript-marionette-v2/src/TodoCollection.ts
@@ -4,6 +4,7 @@ export default class TodoCollection extends Backbone.Collection<TodoModel> {
   model = TodoModel
   comparator = "created"
 
+  // TODO: Consider moving these methods to RootModel.
   allAreCompleted(): boolean {
     return this.all(todo => todo.completed)
   }
