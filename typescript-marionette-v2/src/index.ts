24a55be1a09a54fadafe108ca25f625cf78d9729
Jan Aagaard <jag@zmags.com>
2016-06-24 15:04:22 +0200

    Clean up

--- typescript-marionette-v2/src/index.ts
+++ typescript-marionette-v2/src/index.ts
@@ -1,9 +1,7 @@
 import App from "./App"
 
-// Create our Application
 const app = new App()
 
-// Start history when our application is ready.
 app.on("start", () => {
   Backbone.history.start()
 })
