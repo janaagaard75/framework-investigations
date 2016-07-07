2708215aae95dd75d958a994ab13989cc7c422f8
Jan Aagaard <jag@zmags.com>
2016-07-07 16:40:28 +0200

    Add a couple of TODOs

--- typescript-marionette-v2/src/TodosView.ts
+++ typescript-marionette-v2/src/TodosView.ts
@@ -7,6 +7,7 @@ import TypedCompositeViewOptions from "./TypedCompositeViewOptions"
 interface TodosViewOptions extends TypedCompositeViewOptions<TodoModel, TodoCollection> { }
 
 export default class TodosView extends TypedCompositeView<TodoModel, TodoCollection, TodoView> {
+  // TODO: The view either needs to know about the current filter or it should only be fed with the todos to show.
   constructor(options: TypedCompositeViewOptions<TodoModel, TodoCollection>) {
     super(TodosView.setDefaultOptions(options))
   }
@@ -19,4 +20,9 @@ export default class TodosView extends TypedCompositeView<TodoModel, TodoCollect
     options.childViewContainer = ".jsChildViewContainer"
     return options
   }
+
+  // TODO: This filter function works as expected.
+  // filter(child: TodoModel, index: number, collection: TodoCollection): boolean {
+  //   return child.completed
+  // }
 }
