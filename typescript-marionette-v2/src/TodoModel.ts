efa00e4428f280780681332ec465164384dd339b
Jan Aagaard <jan@aagaard.net>
2016-07-10 18:58:07 +0200

    Remove todo

--- typescript-marionette-v2/src/TodoModel.ts
+++ typescript-marionette-v2/src/TodoModel.ts
@@ -24,7 +24,6 @@ export default class Todo extends Backbone.Model {
     this.set("completed", newValue)
   }
 
-  // TODO: Come up with a better word than title for this property. Description, perhaps?
   get title(): string {
     return this.get("title")
   }
