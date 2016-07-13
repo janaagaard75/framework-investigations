d02089eadf7c352161bd42caa1f53a9fbb5a878c
Jan Aagaard <jan@aagaard.net>
2016-07-13 22:44:21 +0200

    Trying to make the filter work

--- typescript-marionette-v2/src/FilterView.ts
+++ typescript-marionette-v2/src/FilterView.ts
@@ -1,3 +1,4 @@
+import Filter from "./Filter"
 import FilterModel from "./FilterModel"
 import TypedItemView from "./TypedItemView"
 
@@ -20,12 +21,30 @@ export default class FilterView extends TypedItemView<FilterModel> {
     this.setEvents({
       "click @ui.filterLink": this.filterClicked
     })
+
+    this.listenTo(this.options.model, "change:filter", this.render)
   }
 
   template = require("./FilterView.ejs")
 
   private filterClicked() {
     // TODO: Implement
+  }
+
+  private isActive(): boolean {
+    switch (name) {
+      case "Active":
+        return this.options.model.filter === Filter.Active
+
+      case "All":
+        return this.options.model.filter === Filter.All
+
+      case "Completed":
+        return this.options.model.filter === Filter.Completed
+
+      default:
+        throw new Error("Unknown name.")
+    }
   }
 
   private static setDefaultOptions(options: FilterViewOptions): FilterViewOptions {
@@ -35,7 +54,7 @@ export default class FilterView extends TypedItemView<FilterModel> {
 
   templateHelpers() {
     return {
-      active: this.options.active,
+      active: this.isActive(),
       name: this.options.name
     }
   }
