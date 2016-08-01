4bf779ac12fd0a4b16edea0b02658fa4270c4a9b
Jan Aagaard <jag@zmags.com>
2016-08-01 13:52:52 +0200

    Add TODO

--- typescript-marionette-v2/src/views/typedViews/TypedCompositeViewOptions.ts
+++ typescript-marionette-v2/src/views/typedViews/TypedCompositeViewOptions.ts
@@ -1,3 +1,5 @@
+// TODO: Rename this to something about the collection being required. FiltersView does not require it. TypedCompositeView also has to be updated.
+
 interface TypedCompositeViewOptions<
   TModel extends Backbone.Model,
   TCollection extends Backbone.Collection<TModel>
