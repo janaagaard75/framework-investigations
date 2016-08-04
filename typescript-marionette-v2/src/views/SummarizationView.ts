3b778e4b68c67422e42b407f613a0ce217666930
Jan Aagaard <jag@zmags.com>
2016-08-04 16:29:07 +0200

    Fix URL and remove TODO

--- typescript-marionette-v2/src/views/SummarizationView.ts
+++ typescript-marionette-v2/src/views/SummarizationView.ts
@@ -1,11 +1,10 @@
-import SummarizationViewModel from "./SummarizationViewModel"
+import SummarizationViewModel from "../viewModels/SummarizationViewModel"
 import TypedItemView from "./typedViews/TypedItemView"
 import TypedItemViewOptions from "./typedViews/TypedItemViewOptions"
 
 interface SummarizationViewOptions extends TypedItemViewOptions<SummarizationViewModel> {
 }
 
-// TODO: Move the view models to a separate folder?
 export default class SummarizationView extends TypedItemView<SummarizationViewModel> {
   constructor(options: SummarizationViewOptions) {
     super(options)
