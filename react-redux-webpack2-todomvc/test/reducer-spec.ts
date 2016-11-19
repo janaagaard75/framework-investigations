83dcde82b74e3b60c976595152d81d31350d8f81
Jan Aagaard <jan@aagaard.net>
2016-11-19 20:20:34 +0100

    Fix lint command

--- react-redux-webpack2-todomvc/test/reducer-spec.ts
+++ react-redux-webpack2-todomvc/test/reducer-spec.ts
@@ -15,24 +15,25 @@ import {
 describe('todos reducer', () => {
   it('handles add', () => {
     const beforeState: Todos = [{
+      completed: true,
       id: 0,
-      text: '',
-      completed: true
+      text: ''
     }]
 
     const afterState = todosReducer(beforeState, createAddTodo('hello'))
 
     expect(afterState[0]).to.deep.equal({
+      completed: false,
       id: 1,
-      text: 'hello',
-      completed: false
+      text: 'hello'
     })
   })
 
   it('handles delete', () => {
     const beforeState: Todos = [{
+      completed: false,
       id: 1,
-      text: '', completed: false
+      text: ''
     }]
 
     const afterState = todosReducer(beforeState, createDeleteTodo(1))
@@ -42,83 +43,83 @@ describe('todos reducer', () => {
 
   it('handles edit', () => {
     const beforeState: Todos = [{
+      completed: false,
       id: 1,
-      text: '',
-      completed: false
+      text: ''
     }]
 
     const afterState1 = todosReducer(beforeState, createEditTodo({
-      todoId: 1,
-      newText: 'hello'
+      newText: 'hello',
+      todoId: 1
     }))
 
     expect(afterState1[0]).to.deep.equal({
+      completed: false,
       id: 1,
-      text: 'hello',
-      completed: false
+      text: 'hello'
     })
   })
 
   it('handles complete all', () => {
     const state1: Todos = [{
+      completed: false,
       id: 1,
-      text: '',
-      completed: false
+      text: ''
     }]
 
     const state2 = todosReducer(state1, createToggleTodo(1))
 
     expect(state2[0]).to.deep.equal({
+      completed: true,
       id: 1,
-      text: '',
-      completed: true
+      text: ''
     })
 
     const state3 = todosReducer(state2, createToggleTodo(1))
 
     expect(state3[0]).to.deep.equal({
+      completed: false,
       id: 1,
-      text: '',
-      completed: false
+      text: ''
     })
   })
 
   it('handles complete all', () => {
     const state1: Todos = [
-      { id: 1, text: '', completed: false },
-      { id: 2, text: '', completed: true },
-      { id: 3, text: '', completed: false }
+      { completed: false, id: 1, text: '' },
+      { completed: true, id: 2, text: '' },
+      { completed: false, id: 3, text: '' }
     ]
 
     const state2 = todosReducer(state1, createToggleAllTodos())
 
     expect(state2).to.deep.equal([
-      { id: 1, text: '', completed: true },
-      { id: 2, text: '', completed: true },
-      { id: 3, text: '', completed: true }
+      { completed: true, id: 1, text: '' },
+      { completed: true, id: 2, text: '' },
+      { completed: true, id: 3, text: '' }
     ])
 
     const state3 = todosReducer(state2, createToggleAllTodos())
 
     expect(state3).to.deep.equal([
-      { id: 1, text: '', completed: false },
-      { id: 2, text: '', completed: false },
-      { id: 3, text: '', completed: false }
+      { completed: false, id: 1, text: '' },
+      { completed: false, id: 2, text: '' },
+      { completed: false, id: 3, text: '' }
     ])
   })
 
   it('handles clear completed', () => {
     let beforeState: Todos = [
-      { id: 1, text: '', completed: false },
-      { id: 2, text: '', completed: true }
+      { completed: false, id: 1, text: '' },
+      { completed: true, id: 2, text: '' }
     ]
 
     const afterState = todosReducer(beforeState, createClearCompletedTodos())
 
     expect(afterState).to.deep.equal([{
+      completed: false,
       id: 1,
-      text: '',
-      completed: false
+      text: ''
     }])
   })
 })
