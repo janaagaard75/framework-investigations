26f646d85db3dc330d00a0953e790827df59a057
Jan Aagaard <jan@aagaard.net>
2016-11-20 21:22:40 +0100

    Add TODO

--- react-redux-todos/src/containers/VisibleTodoList.ts
+++ react-redux-todos/src/containers/VisibleTodoList.ts
@@ -1,14 +1,19 @@
 import { connect } from 'react-redux'
 
 import { createToggleTodo } from '../actions/createToggleTodo'
+import { Filter } from '../model/Filter'
 import { TodoList } from '../components/TodoList'
+import { Todos } from '../model/Todos'
 
-const getVisibleTodos = (todos, filter) => {
+// TODO: Add types on the parameters.
+const getVisibleTodos = (todos, filter: Filter) => {
   switch (filter) {
     case 'SHOW_ALL':
       return todos
+
     case 'SHOW_COMPLETED':
       return todos.filter(t => t.completed)
+
     case 'SHOW_ACTIVE':
       return todos.filter(t => !t.completed)
   }
