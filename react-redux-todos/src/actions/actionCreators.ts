761a527ce1b2abbe0559438a91a44b648eb91d99
Jan Aagaard <jan@aagaard.net>
2016-12-05 09:09:59 +0100

    Add TODO

--- react-redux-todos/src/actions/actionCreators.ts
+++ react-redux-todos/src/actions/actionCreators.ts
@@ -5,3 +5,5 @@ export const createAddTodo = actionCreatorFactory()<string>('ADD_TODO')
 // TODO: createSetVisibilityFilter is not used. Should it be?
 export const createSetVisibilityFilter = actionCreatorFactory()<Filter>('SET_VISIBILITY_FILTER')
 export const createToggleTodo = actionCreatorFactory()<number>('TOGGLE_TODO')
+
+// TODO: Introduce an asynchroneous action to see how that plays out.
