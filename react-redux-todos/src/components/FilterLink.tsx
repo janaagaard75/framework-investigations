a884c5be028338a0ba27b6965361079957858402
Jan Aagaard <jan@aagaard.net>
2016-12-05 05:32:13 +0100

    Private method

--- react-redux-todos/src/components/FilterLink.tsx
+++ react-redux-todos/src/components/FilterLink.tsx
@@ -4,23 +4,6 @@ import { Component } from 'react'
 import { Filter } from '../model/Filter'
 import { LinkUnlessActive } from '../components/LinkUnlessActive'
 
-const toPathname = (filter: Filter): string => {
-  // TODO: Figure out a better way to associate pathname and filter.
-  switch (filter) {
-    case Filter.ShowAll:
-      return '/'
-
-    case Filter.ShowActive:
-      return '/active'
-
-    case Filter.ShowCompleted:
-      return '/completed'
-
-    default:
-      throw new Error(`The filter '${filter}' is not supported.`)
-  }
-}
-
 interface FilterLinkProps {
   activeFilter: Filter
   filter: Filter
@@ -31,9 +14,26 @@ export class FilterLink extends Component<FilterLinkProps, void> {
     super(props, context)
   }
 
+  private toPathname (filter: Filter): string {
+    // TODO: Figure out a better way to associate pathname and filter.
+    switch (filter) {
+      case Filter.ShowAll:
+        return '/'
+
+      case Filter.ShowActive:
+        return '/active'
+
+      case Filter.ShowCompleted:
+        return '/completed'
+
+      default:
+        throw new Error(`The filter '${filter}' is not supported.`)
+    }
+  }
+
   public render() {
     const isActive = (this.props.filter === this.props.activeFilter)
-    const pathname = toPathname(this.props.filter)
+    const pathname = this.toPathname(this.props.filter)
 
     return (
       <LinkUnlessActive active={isActive} to={pathname}>
