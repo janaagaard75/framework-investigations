510febaed9015b0b8240b31fcd92457f3da531ed
Jan Aagaard <jan@aagaard.net>
2016-12-06 15:21:36 +0100

    Update tests

--- react-redux-todos/test/actions/todos.spec.ts
+++ react-redux-todos/test/actions/todos.spec.ts
@@ -1,31 +1,26 @@
 import { expect } from 'chai'
 
 import { createAddTodo } from '../../src/actions/actionCreators'
-import { createSetVisibilityFilter } from '../../src/actions/actionCreators'
 import { createToggleTodo } from '../../src/actions/actionCreators'
-import { Filter } from '../../src/model/Filter'
 
 // TODO: Fix the tests. They are no longer correct after the filter was converted to an enumeration.
 describe('todo actions', () => {
   it('addTodo should create ADD_TODO action', () => {
-    const actual = createAddTodo('Use Redux')
+    const actual = createAddTodo({ text: 'Use Redux' })
     const expected = {
-      payload: 'Use Redux',
+      payload: {
+        text: 'Use Redux'
+      },
       type: 'ADD_TODO'
     }
     expect(actual).to.deep.equal(expected)
   })
 
-  it('setVisibilityFilter should create SET_VISIBILITY_FILTER action', () => {
-    expect(createSetVisibilityFilter(Filter.ShowActive)).to.deep.equal({
-      payload: Filter.ShowActive,
-      type: 'SET_VISIBILITY_FILTER'
-    })
-  })
-
   it('toggleTodo should create TOGGLE_TODO action', () => {
-    expect(createToggleTodo(1)).to.deep.equal({
-      payload: 1,
+    expect(createToggleTodo({ id: 1 })).to.deep.equal({
+      payload: {
+        id: 1
+      },
       type: 'TOGGLE_TODO'
     })
   })
