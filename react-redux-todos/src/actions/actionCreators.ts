dec5c3a28e648c6db9628e5b90745ea5bc1344e5
Jan Aagaard <jan@aagaard.net>
2016-12-06 13:26:19 +0100

    Started on adding actions for async operation

--- react-redux-todos/src/actions/actionCreators.ts
+++ react-redux-todos/src/actions/actionCreators.ts
@@ -1,5 +1,9 @@
 import actionCreatorFactory from 'redux-typescript-actions'
+
 import { Filter } from '../model/Filter'
+import { TypedDispatch } from '../model/TypedDispatch'
+
+// TODO: Consider always using named parameters for the type of the actions.
 
 export const createAddTodo = actionCreatorFactory()<string>('ADD_TODO')
 // TODO: createSetVisibilityFilter is not used. Should it be?
@@ -7,3 +11,20 @@ export const createSetVisibilityFilter = actionCreatorFactory()<Filter>('SET_VIS
 export const createToggleTodo = actionCreatorFactory()<number>('TOGGLE_TODO')
 
 // TODO: Introduce an asynchronous action to see how that plays out.
+
+export const createAddTodoStarted = actionCreatorFactory()<NodeJS.Timer>('ADD_TODO_STARTED')
+export const createAddTodoDone = actionCreatorFactory()<string>('ADD_TODO_DONE')
+
+export const createAsyncAddTodo = (dispatch: TypedDispatch) => {
+  const timeoutId = setTimeout(() => {
+    dispatch(createAddTodoDone)
+  }, 3 * 1000)
+
+  dispatch(createAddTodoStarted(timeoutId))
+}
+
+// It's possible to queue up adding multiple todos.
+// Adding a todo triggers ADD_TODO_STARTED. The global redux state is updated.
+// Once the timeout is done, the todo will be added to the state.
+// TODO: Random timeout durations.
+// TODO: Simulate an error from time to time.
