73774f241bd9688beb96779c7f381bb504be7f08
Jan Aagaard <jan@aagaard.net>
2016-12-06 13:35:17 +0100

    Only instantiate the factory once

--- react-redux-todos/src/actions/actionCreators.ts
+++ react-redux-todos/src/actions/actionCreators.ts
@@ -3,15 +3,15 @@ import actionCreatorFactory from 'redux-typescript-actions'
 import { Filter } from '../model/Filter'
 import { TypedDispatch } from '../model/TypedDispatch'
 
-export const createAddTodo = actionCreatorFactory()<{text: string}>('ADD_TODO')
+const actionCreator = actionCreatorFactory()
+
+export const createAddTodo = actionCreator<{text: string}>('ADD_TODO')
 // TODO: createSetVisibilityFilter is not used. Should it be?
-export const createSetVisibilityFilter = actionCreatorFactory()<Filter>('SET_VISIBILITY_FILTER')
-export const createToggleTodo = actionCreatorFactory()<{id: number}>('TOGGLE_TODO')
+export const createSetVisibilityFilter = actionCreator<Filter>('SET_VISIBILITY_FILTER')
+export const createToggleTodo = actionCreator<{id: number}>('TOGGLE_TODO')
 
-// TODO: Introduce an asynchronous action to see how that plays out.
-
-export const createAddTodoStarted = actionCreatorFactory()<NodeJS.Timer>('ADD_TODO_STARTED')
-export const createAddTodoDone = actionCreatorFactory()<string>('ADD_TODO_DONE')
+export const createAddTodoStarted = actionCreator<NodeJS.Timer>('ADD_TODO_STARTED')
+export const createAddTodoDone = actionCreator<string>('ADD_TODO_DONE')
 
 export const createAsyncAddTodo = (dispatch: TypedDispatch) => {
   const timeoutId = setTimeout(() => {
@@ -21,6 +21,7 @@ export const createAsyncAddTodo = (dispatch: TypedDispatch) => {
   dispatch(createAddTodoStarted(timeoutId))
 }
 
+// TODO: Introduce an asynchronous action to see how that plays out.
 // It's possible to queue up adding multiple todos.
 // Adding a todo triggers ADD_TODO_STARTED. The global redux state is updated.
 // Once the timeout is done, the todo will be added to the state.
