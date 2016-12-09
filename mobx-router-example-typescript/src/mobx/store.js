d689a6a6deb807de2492ee64a05c742861eb5ccd
Jan Aagaard <jan@aagaard.net>
2016-12-09 16:53:58 +0100

    Remove mobx-router-example

--- mobx-router-example-typescript/src/mobx/store.js
+++
@@ -1,9 +0,0 @@
-import AppStore from 'mobx/stores/app-store';
-import {RouterStore} from 'mobx-router';
-
-const store = {
-  app: new AppStore(),
-  router: new RouterStore()
-};
-
-export default store;
