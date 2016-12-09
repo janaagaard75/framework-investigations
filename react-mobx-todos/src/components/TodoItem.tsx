c5978ed3a817ad111197d43f58eaa527e4470d5c
Jan Aagaard <jan@aagaard.net>
2016-12-09 12:35:09 +0100

    Remove shouldComponentUpdate

--- react-mobx-todos/src/components/TodoItem.tsx
+++ react-mobx-todos/src/components/TodoItem.tsx
@@ -11,12 +11,6 @@ interface TodoItemProps {
 
 @observer
 export class TodoItem extends Component<TodoItemProps, void> {
-  // TODO: This breaks toggling a todo. Not sure why. Removing this means the whole list is repainted when adding a todo. It's as if MobX isn't able to observe that the completed state is changing.
-  // public shouldComponentUpdate(newProps: TodoItemProps) {
-  //   const sameTodo = this.props.todo.equals(newProps.todo)
-  //   return !sameTodo
-  // }
-
   public render() {
     return (
       <li
