7d8ddb2b915955f6c965a77391ccc85111a4b8c3
Jan Aagaard <jan@aagaard.net>
2016-12-09 16:43:56 +0100

    Trying to add routing

--- react-mobx-todos/src/Main.tsx
+++ react-mobx-todos/src/Main.tsx
@@ -1,7 +1,10 @@
 import * as React from "react"
 // TODO: Figure out how to remove dev tools from the production bundle.
 import DevTools from "mobx-react-devtools"
+import { browserHistory } from "react-router"
 import { render } from "react-dom"
+import { Route } from "react-router"
+import { Router } from "react-router"
 import { useStrict } from "mobx"
 
 import { App } from "./components/App"
@@ -14,11 +17,13 @@ declare const process: any
 const includeDevTools = process.env.NODE_ENV === "development"
 
 render(
-  <div>
-    <App store={store}/>
-    {includeDevTools &&
-      <DevTools/>
-    }
-  </div>,
+  <Router history={browserHistory}>
+    <Route component={App} path="/(:filter)">
+      <App store={store}/>
+      {includeDevTools &&
+        <DevTools/>
+      }
+    </Route>
+  </Router>,
   document.getElementById("app")
 )
