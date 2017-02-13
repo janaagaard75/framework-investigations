7e1f81e918fe20b0b886994985439cc71a17934a
Jan Aagaard <jan@aagaard.net>
2017-02-13 22:40:54 +0100

    Replace Babily with UglifyJS

--- react-mobx-todos/webpack.config.js
+++ react-mobx-todos/webpack.config.js
@@ -1,9 +1,9 @@
 // tslint:disable object-literal-sort-keys
-const BabiliPlugin = require("babili-webpack-plugin")
 const CopyWebpackPlugin = require("copy-webpack-plugin")
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 const path = require("path")
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require("webpack")
 
 const nodeEnv = process.env.NODE_ENV || "development"
@@ -36,7 +36,9 @@ const plugins = [
 
 if (isProduction) {
   plugins.push(
-    new BabiliPlugin()
+    new UglifyJSPlugin({
+      sourceMap: true
+    })
   )
 } else {
   plugins.push(
