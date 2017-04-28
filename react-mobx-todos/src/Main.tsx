fc67aa4ea4a0a996fa4bc3eddd5b955d0f90ddc3
Jan Aagaard <jan@aagaard.net>
2017-04-29 00:39:54 +0200

    Make the app show render, but filters still broken

--- react-mobx-todos/src/Main.tsx
+++ react-mobx-todos/src/Main.tsx
@@ -3,15 +3,20 @@ import { BrowserRouter } from 'react-router-dom'
 import { render } from 'react-dom'
 import { Route } from 'react-router'
 
-import { allRoutes } from './routes'
+// import { allRoutes } from './routes'
+import { ConnectedApp } from './ConnectedApp'
 
 import './main.scss'
 
 render(
   <BrowserRouter>
-    {allRoutes.map(route =>
-      <Route key={route.routePath} component={route.component} path={route.routePath}/>
-    )}
+    <div>
+      <Route exact={true} path="/" component={ConnectedApp}/>
+      <Route path="/:filter" component={ConnectedApp}/>
+      {/*{allRoutes.map(route =>
+        <Route key={route.routePath} component={route.component} path={route.routePath}/>
+      )}*/}
+    </div>
   </BrowserRouter>,
   document.getElementById('app')
 )
