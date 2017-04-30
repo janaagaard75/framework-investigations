b1dd2ef1e260bd7dadeda10f1cd37122df3ea27c
Jan Aagaard <jan@aagaard.net>
2017-04-30 20:48:11 +0200

    Switch between routes

--- react-mobx-todos/src/Main.tsx
+++ react-mobx-todos/src/Main.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { render } from 'react-dom'
 import { Route } from 'react-router'
+import { Switch } from 'react-router'
 
 // import { allRoutes } from './routes'
 import { ConnectedApp } from './ConnectedApp'
@@ -11,8 +12,10 @@ import './main.scss'
 render(
   <BrowserRouter>
     <div>
-      <Route exact={true} path="/" component={ConnectedApp}/>
-      <Route path="/:filter" component={ConnectedApp}/>
+      <Switch>
+        <Route exact={true} path="/" component={ConnectedApp}/>
+        <Route path="/:filter" component={ConnectedApp}/>
+      </Switch>
       {/*{allRoutes.map(route =>
         <Route key={route.routePath} component={route.component} path={route.routePath}/>
       )}*/}
