aae7688ff83c0e8c040e4879b3caf330ce620e57
Jan Aagaard <uxj@kmd.dk>
2017-05-08 13:55:30 +0200

    Add markup from template

--- todomvc-react-mobx-typescript/src/components/App.tsx
+++ todomvc-react-mobx-typescript/src/components/App.tsx
@@ -8,11 +8,37 @@ import { TodoList } from './TodoList'
 export class App extends Component<{}, void> {
   public render() {
     return (
-      <section className="todoapp">
-        <section className="main">
-          <TodoList/>
+      <div>
+        <section className="todoapp">
+          <header className="header">
+            <h1>todos</h1>
+            <input className="new-todo" placeholder="What needs to be done?" autoFocus={true} />
+          </header>
+          <section className="main">
+            <TodoList />
+          </section>
+          <footer className="footer">
+            <span className="todo-count"><strong>0</strong> item left</span>
+            <ul className="filters">
+              <li>
+                <a className="selected" href="#/">All</a>
+              </li>
+              <li>
+                <a href="#/active">Active</a>
+              </li>
+              <li>
+                <a href="#/completed">Completed</a>
+              </li>
+            </ul>
+            <button className="clear-completed">Clear completed</button>
+          </footer>
         </section>
-      </section>
+        <footer className="info">
+          <p>Double-click to edit a todo</p>
+          <p>Created by <a href="https://github.com/janaagaard75">Jan Aagaard</a></p>
+          <p>Part of <a href="http://todomvc.com/">TodoMVC</a></p>
+        </footer>
+      </div>
     )
   }
 }
