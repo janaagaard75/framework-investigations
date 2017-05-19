29946f324ae128941c0c8354de6b2642b1f6054d
Jan Aagaard <uxj@kmd.dk>
2017-05-19 11:01:26 +0200

    Add checkbox for toggling all todos

--- todomvc-react-mobx-typescript/src/components/Main.tsx
+++ todomvc-react-mobx-typescript/src/components/Main.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Component } from 'react'
+import { observer } from 'mobx-react'
 
 import { TodoModel } from './TodoModel'
 import { TodoList } from './TodoList'
@@ -9,11 +10,18 @@ interface Props {
   todos: Array<TodoModel>
 }
 
+@observer
 export class Main extends Component<Props, void> {
   public render() {
+    const allTodosChecked = this.props.todos.every(todo => todo.completed)
+
     return (
       <section className="main">
-        <input type="checkbox" className="toggle-all"/>
+        <input
+          className="toggle-all"
+          type="checkbox"
+          checked={allTodosChecked}
+        />
         <label htmlFor="toggle-all">Mark all as complete</label>
         <TodoList
           deleteTodo={this.props.deleteTodo}
