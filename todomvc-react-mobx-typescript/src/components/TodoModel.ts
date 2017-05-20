8a712412068cb90cff448e42bc65a1ababb793d6
Jan Aagaard <jan@aagaard.net>
2017-05-20 19:36:41 +0200

    Make complete writable

--- todomvc-react-mobx-typescript/src/components/TodoModel.ts
+++ todomvc-react-mobx-typescript/src/components/TodoModel.ts
@@ -1,26 +1,20 @@
-import { computed } from 'mobx'
 import { observable } from 'mobx'
 
 export class TodoModel {
   constructor(text: string, completed: boolean) {
-    this._completed = completed
+    this.completed = completed
     this.id = `todo${TodoModel.getNextId()}`
     this.text = text
   }
 
+  @observable public completed: boolean
   public readonly id: string
   public text: string
 
   private static nextId = 0
 
-  @observable private _completed: boolean
-
-  @computed get completed(): boolean {
-    return this._completed
-  }
-
   public toggle(): void {
-    this._completed = !this._completed
+    this.completed = !this.completed
   }
 
   private static getNextId(): number {
